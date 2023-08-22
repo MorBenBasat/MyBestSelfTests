cc7dafec71f17b710b56ed9fcec2e514ac7e392b
mor ben basat <morbb1231@gmail.com>
2023-08-22 14:42:20 +0300

    update init driver

--- ui_testing/initialize_driver.py
+++ ui_testing/initialize_driver.py
@@ -5,5 +5,5 @@ def initialize_driver():
     options = webdriver.ChromeOptions()
     # options.add_argument("--headless")  # Uncomment this line to run in headless mode
 
-    driver = webdriver.Chrome(options=options, executable_path='c:\chromedriver.exe')
+    driver = webdriver.Chrome(options=options)
     return driver
