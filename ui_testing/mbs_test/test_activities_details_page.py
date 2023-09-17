9c03194b9903bebe260c493c3f3a91c7d98e2d92
mor ben basat <morbb1231@gmail.com>
2023-09-17 19:43:27 +0300

    adding new navigation to activity details page

--- ui_testing/mbs_test/test_activities_details_page.py
+++ ui_testing/mbs_test/test_activities_details_page.py
@@ -7,7 +7,7 @@ from pages.Agenda.ActivitiesDetailsPage import ActivitiesDetailsPage
 from pages.Agenda.MyAgendaPage import MyAgendaPage
 from pages.LoginPage import LoginPage
 from pages.Pages_url import PagesUrlMbs
-from test_users.login_users import SuccessLoginUser, InvalidLogin
+from test_users.activities_details_users import ValidActivityDetails
 
 
 class TestActivitiesDetailsPage(unittest.TestCase):
@@ -22,13 +22,11 @@ class TestActivitiesDetailsPage(unittest.TestCase):
 
     @pytest.mark.smoke
     def test_success_navigation_activities_details_page(self):
-        self.login_page.success_login()
         self.activities_details_page.navigate_to_activities_details_page()
-        self.assertEqual(self.driver.current_url, PagesUrlMbs.activities_details, 'Activities Details Page Open!')
+        self.assertEqual(self.driver.current_url, PagesUrlMbs.activities_details, print('Activities Details Page Open!'))
         self.driver.quit()
 
     def test_create_activity(self):
-        self.login_page.success_login()
         self.activities_details_page.navigate_to_activities_details_page()
         self.activities_details_page.fill_all_activities_details('sanitytest', 'sanity test')
         HelpersMbs.delay(2)
@@ -40,7 +38,6 @@ class TestActivitiesDetailsPage(unittest.TestCase):
         self.driver.quit()
 
     def test_no_fill_my_activity(self):
-        self.login_page.success_login()
         self.activities_details_page.navigate_to_activities_details_page()
         self.activities_details_page.fill_all_activities_details('', ' test')
         HelpersMbs.delay(2)
@@ -49,7 +46,6 @@ class TestActivitiesDetailsPage(unittest.TestCase):
         self.driver.quit()
 
     def test_no_fill_why_i_do_this(self):
-        self.login_page.success_login()
         self.activities_details_page.navigate_to_activities_details_page()
         self.activities_details_page.fill_all_activities_details('test', ' ')
         HelpersMbs.delay(2)
@@ -58,8 +54,6 @@ class TestActivitiesDetailsPage(unittest.TestCase):
         self.driver.quit()
 
     def test_no_fill_activities_details_fields(self):
-        self.login_page.success_login()
-
         self.activities_details_page.navigate_to_activities_details_page()
         self.activities_details_page.fill_all_activities_details('', '')
         HelpersMbs.delay(2)
---
+++ ui_testing/test_users/activities_details_users.py
@@ -0,0 +1,8 @@
+class TestActivityDetailsUsers:
+    def __init__(self, activity_name, activity_text, hour):
+        self.activity_name = activity_name
+        self.activity_text = activity_text
+        self.hour = hour
+
+
+ValidActivityDetails = TestActivityDetailsUsers("activity name", "activity text", "04:20")
