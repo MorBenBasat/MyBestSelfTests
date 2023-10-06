c54d2d043a5336694830450d5107b5db86fded46
mor ben basat <morbb1231@gmail.com>
2023-10-06 13:19:42 +0300

    ading tests to recover password page

--- ui_testing/locators/RecoverPasswordPageLocators.py
+++ ui_testing/locators/RecoverPasswordPageLocators.py
@@ -8,3 +8,5 @@ class PasswordRecoverPageLocators:
                             '2]/div/p-button/button'
     ERROR_ALERT = By.XPATH, '/html/body/app-root/div/div/app-recover-password/div/p-card/div/div/div[' \
                             '2]/div/app-form/form/app-input-wrapper/small'
+    REGISTER_BTN = By.XPATH, '/html/body/app-root/div/div/app-recover-password/div/p-card/div/div/div[2]/div/a[2]'
+    LOGIN_BTN = By.XPATH, '/html/body/app-root/div/div/app-recover-password/div/p-card/div/div/div[2]/div/a[1]'
--- ui_testing/pages/RecoverPasswordPage.py
+++ ui_testing/pages/RecoverPasswordPage.py
@@ -19,24 +19,35 @@ class RecoverPasswordPage:
         HelpersMbs.delay(2)
 
     def send_email(self, EmailRecoverPassword):
-        self.send_email_with_click_btn(EmailRecoverPassword)
+        self.send_email_without_click_btn(EmailRecoverPassword)
 
         click_to_send = wait_for_element_visibility(self.driver, *PasswordRecoverPageLocators.CONFIRM_BTN)
         click_to_send.click()
         HelpersMbs.delay(2)
 
-    def send_email_with_click_btn(self, EmailRecoverPassword):
+    def send_email_without_click_btn(self, EmailRecoverPassword):
         fill_email = wait_for_element_visibility(self.driver, *PasswordRecoverPageLocators.EMAIL_FIELD)
         fill_email.send_keys(EmailRecoverPassword.email)
         HelpersMbs.delay(1)
 
-    def invalid_email_login(self,EmailRecoverPassword):
-        self.send_email_with_click_btn(EmailRecoverPassword)
+    def invalid_email_login(self, EmailRecoverPassword):
+        self.send_email_without_click_btn(EmailRecoverPassword)
         invalid_email = wait_for_element_visibility(self.driver, *PasswordRecoverPageLocators.ERROR_ALERT)
         if invalid_email.text == "נא להזין מייל חוקי":
-
             print("מופיע נא להזין מייל חוקי")
-            return "נא להזין מייל חוקי"  # Return the message for validation in the test case
+            return "נא להזין מייל חוקי"
         else:
             print("Unexpected message:", invalid_email.text)
             return None
+
+    def click_register_btn(self):
+        self.navigation_to_recover_password_page()
+        register_btn = wait_for_element_clickable(self.driver, *PasswordRecoverPageLocators.REGISTER_BTN)
+        register_btn.click()
+        HelpersMbs.delay(1)
+
+    def click_login_btn(self):
+        self.navigation_to_recover_password_page()
+        login_btn = wait_for_element_clickable(self.driver, *PasswordRecoverPageLocators.LOGIN_BTN)
+        login_btn.click()
+        HelpersMbs.delay(1)
