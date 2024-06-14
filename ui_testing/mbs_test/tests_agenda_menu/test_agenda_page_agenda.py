4111e566d348d71d392265de391b09ddee17e152
MorBenBasat <morbb1231@gmail.com>
2024-06-14 11:16:43 +0300

    Adding test test_verify_default_day_today

--- ui_testing/mbs_test/tests_agenda_menu/test_agenda_page_agenda.py
+++ ui_testing/mbs_test/tests_agenda_menu/test_agenda_page_agenda.py
@@ -29,16 +29,11 @@ class TestActivitiesPage(unittest.TestCase):
         self.assertEqual(self.driver.current_url, PagesUrlMbs.agenda, print(AgendaPageOpenByDropList.my_string))
 
     def test_verify_default_day_today(self):
-        # Set the locale to Hebrew (Israel)
-        locale.setlocale(locale.LC_TIME, 'he_IL.utf8')  # Adjust if needed based on your system's locale settings
-
         self.login_page.success_login()
         self.agenda_page.navigate_to_agenda_page()
 
-        # Get today's date formatted as "14 ביוני 2024"
-        today_date = datetime.now().strftime('%d ב%B %Y')
+        today_date = HelpersMbs.get_today_date_in_hebrew()
 
-        # Print today's date for debugging
         print(f"Expected date: {today_date}")
 
         # Fetch the actual day text from the page
@@ -47,7 +42,6 @@ class TestActivitiesPage(unittest.TestCase):
         # Print actual day text for debugging
         print(f"Actual day text: {actual_text}")
 
-        # Assert that the actual day text matches today's date
-        self.assertEqual(today_date, actual_text, "The displayed day does not match today's date")
+        self.assertEqual(actual_text, today_date, )
 
         self.driver.quit()
